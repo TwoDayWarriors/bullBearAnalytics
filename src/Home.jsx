a8bd7921439cc6881188de2aaef957814d2c790d
Karina Velasquez <kvelasquez@xpanxion.com>
2023-08-18 21:44:34 -0400

    LoginFunctional

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,7 +6,6 @@ const Home = () => {
   return (
     <div className="home">
       <h2>Welcome to BullBearAnalytics</h2>
-      <p>Your home page content goes here...</p>
       <img src={homeImage} alt="Home" /> {/* Use the imported image */}
       
       
